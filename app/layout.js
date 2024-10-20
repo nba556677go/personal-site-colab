15835c8bb03c05d9be5686c58c9695acb4d7f75a
nba556677go <nba556677@gmail.com>
2024-10-20 16:15:41 -0400

    update icon

--- app/layout.js
+++ app/layout.js
@@ -26,13 +26,13 @@ export default function RootLayout({ children }) {
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
+            href="/static/favicons/SBU-vert_72icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
+            href="/static/favicons/SBU-vert_72icon.png"
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
