6f60a9ad086301c57afffa507547da7f9ad08e2b
nba556677go <nba556677@gmail.com>
2024-10-20 00:42:13 -0400

    update pub

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -4,7 +4,6 @@ import { usePathname } from 'next/navigation';
 export default function Publications() {
   return (
     <div>
-      <h2>Publications</h2>
       <div className="publication-entry">
         <span className="badge">SoCC '24</span> {/* You can update this label as per your conference */}
         <h3>
@@ -13,14 +12,15 @@ export default function Publications() {
         <p>
           Bing-Shiun Han, Tathagata Paul, Zhenhua Liu, Anshul Gandhi
         </p>
-        <p>In Proceedings of the 2024 ACM Symposium on Cloud Computing (SoCC 2024).</p>
+        <p><em>In Proceedings of the 2024 ACM Symposium on Cloud Computing (SoCC 2024).</em></p>
+        <p> </p>
         <div className="publication-links">
           <a href="pdf-url" className="button">PDF</a>
           <a href="preprint-url" className="button">Preprint</a>
           <a href="cite-url" className="button">Cite</a>
-          <a href="code-url" className="button">Code</a>
-          <a href="slides-url" className="button">Slides</a>
-          <a href="video-url" className="button">Video</a>
+          
+          <a href="https://github.com/nba556677go/KACE-artifact" className="button">Code</a>
+
         </div>
       </div>
     </div>
